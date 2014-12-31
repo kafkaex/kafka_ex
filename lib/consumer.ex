7be7678b465cade07117304a6a6f08bc47ef6d44
Jack Lund <jackl@geekheads.net>
2014-12-30 19:26:40 -0600

    Changed metadata return to be two maps

--- lib/consumer.ex
+++ lib/consumer.ex
@@ -2,6 +2,6 @@ defmodule Kafka.Consumer do
   use GenServer
 
   def init({broker_list, client_id}) do
-    IO.inspect(Kafka.Connection.connect(broker_list, client_id))
+    {:ok, IO.inspect(Kafka.Connection.connect(broker_list, client_id))}
   end
 end
--- lib/metadata.ex
+++ lib/metadata.ex
@@ -6,79 +6,53 @@ defmodule Kafka.Metadata do
 
   def parse_response(metadata) do
     << num_brokers :: 32, rest :: binary >> = metadata
-    case parse_broker_list(num_brokers, rest) do
-      {:ok, brokers, rest} ->
-        << num_topic_metadata :: 32, rest :: binary >> = rest
-        case parse_topic_metadata(num_topic_metadata, rest) do
-          {:ok, topic_metadata} -> {:ok, brokers, topic_metadata}
-          error                 -> error
-        end
-      error                ->
-        IO.puts("Error - #{error}")
-        error
-    end
+    {broker_map, rest} = parse_broker_list(%{}, num_brokers, rest)
+    << num_topic_metadata :: 32, rest :: binary >> = rest
+    {topic_map, _} = parse_topic_metadata(%{}, num_topic_metadata, rest)
+    {broker_map, topic_map}
   end
 
-  defp parse_broker_list(0, rest) do
-    {:ok, [], rest}
+  defp parse_broker_list(map, 0, rest) do
+    {map, rest}
   end
 
-  defp parse_broker_list(num_brokers, data) do
+  defp parse_broker_list(map, num_brokers, data) do
     << node_id :: 32, host_len :: 16, host :: size(host_len)-binary, port :: 32, rest :: binary >> = data
-    case parse_broker_list(num_brokers-1, rest) do
-      {:ok, list, rest} -> {:ok, [%{node_id: node_id, host: host, port: port} | list], rest}
-      error             -> error
-    end
+    {broker_map, rest} = parse_broker_list(map, num_brokers-1, rest)
+    {Map.put(broker_map, node_id, %{host: host, port: port}), rest}
   end
 
-  defp parse_topic_metadata(0, _) do
-    {:ok, []}
+  defp parse_topic_metadata(map, 0, rest) do
+    {map, rest}
   end
 
-  defp parse_topic_metadata(num_topic_metadata, data) do
+  defp parse_topic_metadata(map, num_topic_metadata, data) do
     << error_code :: 16, topic_len :: 16, topic :: size(topic_len)-binary, num_partitions :: 32, rest :: binary >> = data
-    case parse_partition_metadata(num_partitions, rest) do
-      {:ok, partition_metadata, rest} ->
-        case parse_topic_metadata(num_topic_metadata-1, rest) do
-          {:ok, topic_metadata} ->
-            {:ok, [%{topic: topic, error_code: error_code, partition_metadata: partition_metadata} | topic_metadata]}
-          error                 -> error
-        end
-      error                           -> error
-    end
+    {partition_map, rest} = parse_partition_metadata(%{}, num_partitions, rest)
+    {topic_map, rest} = parse_topic_metadata(map, num_topic_metadata-1, rest)
+    {Map.put(topic_map, topic, error_code: error_code, partitions: partition_map), rest}
   end
 
-  defp parse_partition_metadata(0, _) do
-    {:ok, [], nil}
+  defp parse_partition_metadata(map, 0, rest) do
+    {map, rest}
   end
 
-  defp parse_partition_metadata(num_partitions, data) do
+  defp parse_partition_metadata(map, num_partitions, data) do
     << error_code :: 16, id :: 32, leader :: 32, num_replicas :: 32, rest :: binary >> = data
-    case parse_int32_array(num_replicas, rest) do
-      {:ok, replicas, rest} ->
-        << num_isr :: 32, rest :: binary >> = rest
-        case parse_int32_array(num_isr, rest) do
-          {:ok, isrs, rest} ->
-            case parse_partition_metadata(num_partitions-1, rest) do
-              {:ok, partition_metadata, rest} ->
-                {:ok, [%{partition_id: id, error_code: error_code, leader: leader, replicas: replicas, isrs: isrs} | partition_metadata], rest}
-              error                     -> error
-            end
-          error             -> error
-        end
-      error                 -> error
-    end
+    {replicas, rest} = parse_int32_array(num_replicas, rest)
+    << num_isr :: 32, rest :: binary >> = rest
+    {isrs, rest} = parse_int32_array(num_isr, rest)
+    {partition_map, rest} = parse_partition_metadata(map, num_partitions-1, rest)
+    {Map.put(partition_map, id, %{error_code: error_code, leader: leader, replicas: replicas, isrs: isrs}), rest}
   end
 
   defp parse_int32_array(0, rest) do
-    {:ok, [], rest}
+    {[], rest}
   end
 
   defp parse_int32_array(num, data) do
     << value :: 32, rest :: binary >> = data
-    case parse_int32_array(num-1, rest) do
-      {:ok, values, rest} -> {:ok, [value | values], rest}
-      error         -> error
-    end
+    {values, rest} = parse_int32_array(num-1, rest)
+    {[value | values], rest}
   end
 end
