7910edc9fc28f2840ebdd725ad505f2a3eb20dba
Jack Lund <jackl@geekheads.net>
2015-01-23 22:04:39 -0600

    Refactored protocol code and added tests

--- lib/protocol/metadata.ex
+++ lib/protocol/metadata.ex
@@ -3,56 +3,99 @@ defmodule Kafka.Protocol.Metadata do
     Kafka.Protocol.create_request(:metadata, connection) <> << 0 :: 32 >>
   end
 
-  def parse_response(connection, metadata) do
+  def parse_response(connection, << _ :: 32, num_brokers :: 32, rest :: binary >>) do
     timestamp = Kafka.Helper.get_timestamp
-    << _ :: 32, num_brokers :: 32, rest :: binary >> = metadata
-    {broker_map, rest} = parse_broker_list(%{}, num_brokers, rest)
-    << num_topic_metadata :: 32, rest :: binary >> = rest
-    {topic_map, _} = parse_topic_metadata(%{}, num_topic_metadata, rest)
+    parse_broker_list(%{}, num_brokers, rest)
+    |> parse_topic_metadata_list
+    |> generate_result(timestamp, connection)
+  end
+
+  def parse_response(_connection, _data) do
+    {:error, "Parse error: number of brokers not found"}
+  end
+
+  defp generate_result({:ok, broker_map, topic_map, rest}, timestamp, connection) do
     {:ok, %{brokers: broker_map, topics: topic_map, timestamp: timestamp, connection: connection}}
   end
 
+  defp generate_result({:error, message}, _ts, _connection) do
+    {:error, message}
+  end
+
   defp parse_broker_list(map, 0, rest) do
-    {map, rest}
+    {:ok, map, rest}
   end
 
-  defp parse_broker_list(map, num_brokers, data) do
-    << node_id :: 32, host_len :: 16, host :: size(host_len)-binary, port :: 32, rest :: binary >> = data
-    {broker_map, rest} = parse_broker_list(map, num_brokers-1, rest)
-    {Map.put(broker_map, node_id, %{host: host, port: port, socket: nil}), rest}
+  defp parse_broker_list(map, num_brokers, << node_id :: 32, host_len :: 16, host :: size(host_len)-binary, port :: 32, rest :: binary >>) do
+    parse_broker_list(Map.put(map, node_id, %{host: host, port: port, socket: nil}), num_brokers-1, rest)
   end
 
-  defp parse_topic_metadata(map, 0, rest) do
-    {map, rest}
+  defp parse_broker_list(_map, _brokers, _data) do
+    {:error, "Parse error: bad broker list data"}
   end
 
-  defp parse_topic_metadata(map, num_topic_metadata, data) do
-    << error_code :: 16, topic_len :: 16, topic :: size(topic_len)-binary, num_partitions :: 32, rest :: binary >> = data
-    {partition_map, rest} = parse_partition_metadata(%{}, num_partitions, rest)
-    {topic_map, rest} = parse_topic_metadata(map, num_topic_metadata-1, rest)
-    {Map.put(topic_map, topic, error_code: error_code, partitions: partition_map), rest}
+  defp parse_topic_metadata_list({:ok, broker_map, << num_topic_metadata :: 32, rest :: binary >>}) do
+    parse_topic_metadata(%{}, broker_map, num_topic_metadata, rest)
+  end
+
+  defp parse_topic_metadata_list({:error, message}, _map) do
+    {:error, message}
+  end
+
+  defp parse_topic_metadata(map, broker_map, 0, rest) do
+    {:ok, broker_map, map, rest}
+  end
+
+  defp parse_topic_metadata(map, broker_map, num_topic_metadata, << error_code :: 16, topic_len :: 16, topic :: size(topic_len)-binary, num_partitions :: 32, rest :: binary >>) do
+    case parse_partition_metadata(%{}, num_partitions, rest) do
+      {:ok, partition_map, rest} ->
+        parse_topic_metadata(Map.put(map, topic, error_code: error_code, partitions: partition_map), broker_map, num_topic_metadata-1, rest)
+      {:error, message} ->
+        {:error, message}
+    end
+  end
+
+  defp parse_topic_metadata(_map, _num, _data) do
+    {:error, "Parse error: bad topic metadata"}
   end
 
   defp parse_partition_metadata(map, 0, rest) do
-    {map, rest}
+    {:ok, map, rest}
   end
 
-  defp parse_partition_metadata(map, num_partitions, data) do
-    << error_code :: 16, id :: 32, leader :: 32, num_replicas :: 32, rest :: binary >> = data
-    {replicas, rest} = parse_int32_array(num_replicas, rest)
-    << num_isr :: 32, rest :: binary >> = rest
-    {isrs, rest} = parse_int32_array(num_isr, rest)
-    {partition_map, rest} = parse_partition_metadata(map, num_partitions-1, rest)
-    {Map.put(partition_map, id, %{error_code: error_code, leader: leader, replicas: replicas, isrs: isrs}), rest}
+  defp parse_partition_metadata(map, num_partitions, << error_code :: 16, id :: 32, leader :: 32, rest :: binary >>) do
+    case parse_replicas_and_isrs(rest) do
+      {:ok, replicas, isrs, rest} ->
+        parse_partition_metadata(Map.put(map, id, %{error_code: error_code, leader: leader, replicas: replicas, isrs: isrs}), num_partitions-1, rest)
+      {:error, message} -> {:error, message}
+    end
   end
 
-  defp parse_int32_array(0, rest) do
-    {[], rest}
+  defp parse_replicas_and_isrs(<< num_replicas :: 32, rest :: binary >>) do
+    parse_int32_array([], num_replicas, rest)
+    |> parse_isrs
   end
 
-  defp parse_int32_array(num, data) do
-    << value :: 32, rest :: binary >> = data
-    {values, rest} = parse_int32_array(num-1, rest)
-    {[value | values], rest}
+  defp parse_isrs({:ok, replicas, << num_isrs :: 32, rest ::binary >>}) do
+    case parse_int32_array([], num_isrs, rest) do
+      {:ok, isrs, rest} -> {:ok, replicas, isrs, rest}
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  defp parse_isrs({:error, message}) do
+    {:error, message}
+  end
+
+  defp parse_int32_array(array, 0, rest) do
+    {:ok, array, rest}
+  end
+
+  defp parse_int32_array(array, num, << value :: 32, rest :: binary >>) do
+    parse_int32_array([value | array], num-1, rest)
+  end
+
+  defp parse_int32_array(_array, _num, _data) do
+    {:error, "parse error"}
   end
 end
---
+++ test/protocol/metadata_test.exs
@@ -0,0 +1,27 @@
+defmodule Kafka.Protocol.Metadata.Test do
+  use ExUnit.Case, async: true
+  import Mock
+
+  test "create_request creates a valid metadata request" do
+    good_request = << 3 :: 16, 0 :: 16, 1 :: 32, 3 :: 16, "foo" :: binary, 0 :: 32 >>
+    request = Kafka.Protocol.Metadata.create_request(%{correlation_id: 1, client_id: "foo"})
+    assert request == good_request
+  end
+
+  test "parse_response correctly parses a valid response" do
+    response = << 0 :: 32, 1 :: 32, 0 :: 32, 3 :: 16, "foo" :: binary, 9092 :: 32, 1 :: 32, 0 :: 16, 3 :: 16, "bar" :: binary,
+      1 :: 32, 0 :: 16, 0 :: 32, 0 :: 32, 0 :: 32, 1 :: 32, 0 :: 32 >>
+    assert {:ok,
+      %{brokers: %{0 => %{host: "foo", port: 9092, socket: nil}},
+        connection: %{client_id: "foo", correlation_id: 1}, timestamp: _,
+        topics: %{"bar" => [error_code: 0,
+            partitions: %{0 => %{error_code: 0, isrs: [0], leader: 0,
+                replicas: []}}]}}} = Kafka.Protocol.Metadata.parse_response(%{correlation_id: 1, client_id: "foo"}, response)
+  end
+
+  test "parse_response returns an error parsing an invalid response" do
+    response = << 0 :: 32, 1 :: 32, 0 :: 32, 3 :: 16, "foo" :: binary, 9092 :: 32, 1 :: 32, 0 :: 16, 3 :: 16, "bar" :: binary,
+      1 :: 16, 0 :: 16, 0 :: 32, 0 :: 32, 0 :: 32, 1 :: 32, 0 :: 32 >>
+    assert {:error, _} = Kafka.Protocol.Metadata.parse_response(%{correlation_id: 1, client_id: "foo"}, response)
+  end
+end
