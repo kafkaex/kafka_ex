a7eec84ff2136709c796cbd3e11fba426a32c21d
Jack Lund <jackl@geekheads.net>
2015-01-30 18:07:19 -0600

    Added SimpleConsumer.fetch

--- lib/simple_consumer.ex
+++ lib/simple_consumer.ex
@@ -8,32 +8,42 @@ defmodule Kafka.SimpleConsumer do
 
   def fetch(consumer, offset, wait_time \\ 10, min_bytes \\ 1, max_bytes \\ 1_000_000)
 
-  def fetch(consumer, offset, wait_time, min_bytes, max_bytes) do
-    Kafka.Metadata.update(consumer.metadata)
-    |> fetch(consumer, offset, wait_time, min_bytes, max_bytes)
-  end
-
   def fetch({:ok, consumer}, offset, wait_time, min_bytes, max_bytes) do
     fetch({:ok, :cached, consumer.metadata}, consumer, offset, wait_time, min_bytes, max_bytes)
   end
 
   def fetch({:ok, :cached, metadata}, consumer, offset, wait_time, min_bytes, max_bytes) do
-    Kafka.Connection.send(consumer.connection,
-      Kafka.Protocol.Fetch.create_request(consumer.connection, consumer.topic, consumer.partition, offset, wait_time, min_bytes, max_bytes))
+    case Kafka.Connection.send(consumer.connection,
+      Kafka.Protocol.Fetch.create_request(consumer.connection, consumer.topic, consumer.partition, offset, wait_time, min_bytes, max_bytes)) do
+        {:ok, _, response} -> {:ok, consumer, response}
+        {:error, reason}   -> {:error, reason}
+    end
   end
 
   def fetch({:ok, :updated, metadata}, consumer, offset, wait_time, min_bytes, max_bytes) do
-    broker = Kafka.Metadata.get_broker(metadata, consumer.topic, consumer.partition)
-    cond do
-      broker != consumer.broker ->
-        get_broker({:ok, metadata}, consumer.topic, consumer.partition)
-        |> connect(consumer.connection.client_id)
-        |> update_consumer(consumer, consumer.topic, consumer.partition)
-        |> fetch(offset, wait_time, min_bytes, max_bytes)
+    case Kafka.Metadata.get_broker(metadata, consumer.topic, consumer.partition) do
+      {:ok, broker, metadata} ->
+        cond do
+          broker != consumer.broker ->
+            connect({:ok, broker, metadata}, consumer.connection.client_id)
+            |> update_consumer(consumer, consumer.topic, consumer.partition)
+            |> fetch(offset, wait_time, min_bytes, max_bytes)
 
-      true ->
-        fetch({:ok, :cached, metadata}, consumer, offset, wait_time, min_bytes, max_bytes)
+          true ->
+            fetch({:ok, :cached, metadata}, consumer, offset, wait_time, min_bytes, max_bytes)
+        end
+
+        {:error, reason} -> {:error, reason}
     end
+  end
+
+  def fetch({:error, reason}, _offset, _wait_time, _min_bytes, _max_bytes) do
+    {:error, reason}
+  end
+
+  def fetch(consumer, offset, wait_time, min_bytes, max_bytes) do
+    Kafka.Metadata.update(consumer.metadata)
+    |> fetch(consumer, offset, wait_time, min_bytes, max_bytes)
   end
 
   defp create_consumer({:ok, connection, metadata, broker}, topic, partition) do
@@ -53,7 +63,7 @@ defmodule Kafka.SimpleConsumer do
     {:ok, %{consumer | :metadata => metadata, :broker => broker, :connection => connection}}
   end
 
-  defp update_consumer({:error, reason, nil}, _topic, _partition) do
+  defp update_consumer({:error, reason, _}, _topic, _partition) do
     {:error, reason}
   end
 
@@ -62,6 +72,7 @@ defmodule Kafka.SimpleConsumer do
   end
 
   defp connect({:ok, broker, metadata}, client_id) do
+    IO.puts("Calling Kafka.Connection.connect")
     case Kafka.Connection.connect(broker, client_id) do
       {:ok, connection} -> {:ok, connection, metadata, broker}
       {:error, reason}  -> {:error, reason, metadata}
