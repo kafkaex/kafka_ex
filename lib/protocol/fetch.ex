338039e1040f4b061e3f1da33e53bd779dff3bfa
Jack Lund <jackl@geekheads.net>
2015-01-31 16:18:53 -0600

    Moved some message set stuff to Util

--- lib/protocol/fetch.ex
+++ lib/protocol/fetch.ex
@@ -42,7 +42,7 @@ defmodule Kafka.Protocol.Fetch do
   end
 
   defp parse_partitions(map, num_partitions, << partition :: 32, error_code :: 16, hw_mark_offset :: 64, msg_set_size :: 32, msg_set_data :: size(msg_set_size)-binary, rest :: binary >>) do
-    case parse_message_set([], msg_set_data) do
+    case Kafka.Util.parse_message_set([], msg_set_data) do
       {:ok, message_set} ->
         parse_partitions(Map.put(map, partition, %{:error_code => error_code, :hw_mark_offset => hw_mark_offset, :message_set => message_set}), num_partitions-1, rest)
       {:error, message}        -> {:error, message}
@@ -51,48 +51,5 @@ defmodule Kafka.Protocol.Fetch do
 
   defp parse_partitions(_map, _num, data) do
     {:error, "Error parsing partition data in fetch response", data}
-  end
-
-  defp parse_message_set(list, << >>) do
-    {:ok, list}
-  end
-
-  defp parse_message_set(list, << offset :: 64, msg_size :: 32, msg_data :: size(msg_size)-binary, rest :: binary >>) do
-    case parse_message(msg_data) do
-      {:ok, message} -> parse_message_set(Enum.concat(list, [Map.put(message, :offset, offset)]), rest)
-      {:error, error_message} -> {:error, error_message}
-    end
-  end
-
-  defp parse_message_set(_map, data) do
-    {:error, "Error parsing message set in fetch response", data}
-  end
-
-  defp parse_message(<< crc :: 32, _magic :: 8, attributes :: 8, rest :: binary>>) do
-    parse_key(crc, attributes, rest)
-  end
-
-  defp parse_key(crc, attributes, << -1 :: 32-signed, rest :: binary >>) do
-    parse_value(crc, attributes, nil, rest)
-  end
-
-  defp parse_key(crc, attributes, << key_size :: 32, key :: size(key_size)-binary, rest :: binary >>) do
-    parse_value(crc, attributes, key, rest)
-  end
-
-  defp parse_key(_crc, _attributes, data) do
-    {:error, "Error parsing key from message in fetch response", data}
-  end
-
-  defp parse_value(crc, attributes, key, << -1 :: 32-signed >>) do
-    {:ok, %{:crc => crc, :attributes => attributes, :key => key, :value => nil}}
-  end
-
-  defp parse_value(crc, attributes, key, << value_size :: 32, value :: size(value_size)-binary >>) do
-    {:ok, %{:crc => crc, :attributes => attributes, :key => key, :value => value}}
-  end
-
-  defp parse_value(_crc, _attributes, _key, data) do
-    {:error, "Error parsing value from message in fetch response", data}
   end
 end
--- lib/util.ex
+++ lib/util.ex
@@ -40,4 +40,47 @@ defmodule Kafka.Util do
         end
     end
   end
+
+  def parse_message_set(list, << >>) do
+    {:ok, list}
+  end
+
+  def parse_message_set(list, << offset :: 64, msg_size :: 32, msg_data :: size(msg_size)-binary, rest :: binary >>) do
+    case parse_message(msg_data) do
+      {:ok, message} -> parse_message_set(Enum.concat(list, [Map.put(message, :offset, offset)]), rest)
+      {:error, error_message} -> {:error, error_message}
+    end
+  end
+
+  def parse_message_set(_map, data) do
+    {:error, "Error parsing message set in fetch response", data}
+  end
+
+  def parse_message(<< crc :: 32, _magic :: 8, attributes :: 8, rest :: binary>>) do
+    parse_key(crc, attributes, rest)
+  end
+
+  def parse_key(crc, attributes, << -1 :: 32-signed, rest :: binary >>) do
+    parse_value(crc, attributes, nil, rest)
+  end
+
+  def parse_key(crc, attributes, << key_size :: 32, key :: size(key_size)-binary, rest :: binary >>) do
+    parse_value(crc, attributes, key, rest)
+  end
+
+  def parse_key(_crc, _attributes, data) do
+    {:error, "Error parsing key from message in fetch response", data}
+  end
+
+  def parse_value(crc, attributes, key, << -1 :: 32-signed >>) do
+    {:ok, %{:crc => crc, :attributes => attributes, :key => key, :value => nil}}
+  end
+
+  def parse_value(crc, attributes, key, << value_size :: 32, value :: size(value_size)-binary >>) do
+    {:ok, %{:crc => crc, :attributes => attributes, :key => key, :value => value}}
+  end
+
+  def parse_value(_crc, _attributes, _key, data) do
+    {:error, "Error parsing value from message in fetch response", data}
+  end
 end
