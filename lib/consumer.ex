e7dcdf43112b9c6bd662ffbb298f507ac7a67ff3
bjhaid and jacklund <code@getbraintree.com>
2015-02-06 23:10:43 +0000

    Move files around.

--- lib/consumer.ex
+++
@@ -1,9 +0,0 @@
-defmodule Kafka.Consumer do
-  def new(broker_list, client_id) do
-    {:ok, %{:broker_list => broker_list, :client_id => client_id}}
-  end
-
-  def subscribe(consumer, handler, topic, partition \\ :all, offset \\ :latest) do
-    Task.start_link(Kafka.Consumer.Worker, :start, [consumer.broker_list, consumer.client_id, handler, topic, partition, offset])
-  end
-end
--- lib/kafka/util.ex
+++ lib/kafka/util.ex
@@ -1,23 +1,4 @@
 defmodule Kafka.Util do
-  def get_broker({:ok, metadata}, topic, partition) do
-    Kafka.Metadata.get_broker(metadata, topic, partition)
-  end
-
-  def get_broker({:error, reason}, _topic, _partition) do
-    {:error, reason, nil}
-  end
-
-  def connect({:error, reason, metadata}, _client_id) do
-    {:error, reason, metadata}
-  end
-
-  def connect({:ok, broker, metadata}, client_id) do
-    case Kafka.Connection.connect(broker, client_id) do
-      {:ok, connection} -> {:ok, connection, metadata, broker}
-      {:error, reason}  -> {:error, reason, metadata}
-    end
-  end
-
   def create_message_set(value, key \\ nil) do
     message = create_message(value, key)
     << 0 :: 64, byte_size(message) :: 32 >> <> message
