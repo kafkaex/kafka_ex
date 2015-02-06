9de45b3f8f39f9b1aca607fdb276e4a9c0eb1fdc
bjhaid and jacklund <code@getbraintree.com>
2015-02-06 23:44:16 +0000

    Cleanup

--- lib/kafka.ex
+++ lib/kafka.ex
@@ -2,6 +2,6 @@ defmodule Kafka do
   use Application
 
   def start(_type, _args) do
-    Kafka.Supervisor.start_link()
+    Kafka.Supervisor.start_link
   end
 end
--- mix.exs
+++ mix.exs
@@ -8,22 +8,9 @@ defmodule Kafka.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:mock, ">= 0.1.0"}]
   end
