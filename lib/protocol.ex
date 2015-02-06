1efb8dd982f744774012d79a28cc7db71ff996f7
Jack Lund <jackl@geekheads.net>
2015-02-05 21:37:21 -0600

    Added protocol error codes

--- lib/protocol.ex
+++ lib/protocol.ex
@@ -26,4 +26,30 @@ defmodule Kafka.Protocol do
     << api_key(type) :: 16, @api_version :: 16, connection.correlation_id :: 32,
        byte_size(connection.client_id) :: 16, connection.client_id :: binary >>
   end
+
+  @error_map %{
+     0 => :no_error,
+     1 => :offset_out_of_range,
+     2 => :invalid_message,
+     3 => :unknown_topic_or_partition,
+     4 => :invalid_message_size,
+     5 => :leader_not_available,
+     6 => :not_leader_for_partition,
+     7 => :request_timed_out,
+     8 => :broker_not_available,
+     9 => :replica_not_available,
+    10 => :message_size_too_large,
+    11 => :stale_controller_epoch,
+    12 => :offset_metadata_too_large,
+    14 => :offset_loads_in_progress,
+    15 => :consumer_coordinator_not_available,
+    16 => :not_coordinator_for_consumer
+  }
+
+  def error(err_no) do
+    case err_no do
+      -1 -> :unknown_error
+      _  -> @error_map[err_no] || err_no
+    end
+  end
 end
