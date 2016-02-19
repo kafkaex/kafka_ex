bb644eef4eb548411cc4869f78e40c9789cb8a39
Dan Swain <dan.t.swain@gmail.com>
2016-02-19 15:38:29 -0500

    Add test for disable_default_worker

---
+++ test/kafka_ex_test.exs
@@ -0,0 +1,25 @@
+defmodule KafkaExTest do
+  use ExUnit.Case, async: false
+
+  test "Setting disable_default_worker to true removes the KafkaEx.Server worker" do
+    # stop the application, disable the default worker, restart the application
+    :ok = Application.stop(:kafka_ex)
+    Application.put_env(:kafka_ex, :disable_default_worker, true)
+    {:ok, _} = Application.ensure_all_started(:kafka_ex)
+
+    # the supervisor should now have no children and the default worker should not be registered
+    assert [] == Supervisor.which_children(KafkaEx.Supervisor)
+    assert nil == Process.whereis(KafkaEx.Server)
+
+    # revert the change, restart the application
+    :ok = Application.stop(:kafka_ex)
+    Application.put_env(:kafka_ex, :disable_default_worker, false)
+    {:ok, _} = Application.ensure_all_started(:kafka_ex)
+
+    # we should have the default worker back again
+    pid = Process.whereis(KafkaEx.Server)
+    assert is_pid(pid)
+    assert [{:undefined, pid, :worker, [KafkaEx.Server]}] ==
+      Supervisor.which_children(KafkaEx.Supervisor)
+  end
+end
