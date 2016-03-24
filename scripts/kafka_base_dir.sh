f73e3e170e06f144233919d8dded9abd2240642c
Dan Swain <dan.t.swain@gmail.com>
2016-03-23 20:43:40 -0400

    Try not using BASH_SOURCE

--- scripts/kafka_base_dir.sh
+++ scripts/kafka_base_dir.sh
@@ -2,6 +2,6 @@ if [ "$TRAVIS" = "true" ]
 then
   base=~/kafka
 else
-  project_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
+  project_root="$( cd "$( dirname $0 )"/.. && pwd )"
   base=${project_root}/_build/kafka/dist
 fi
