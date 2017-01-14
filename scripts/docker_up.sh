3ebb8a878412acd2a35a9d026a1c3a60f5abec1f
Dan Swain <dan.t.swain@gmail.com>
2017-01-14 11:56:59 -0500

    :rage3:

--- scripts/docker_up.sh
+++ scripts/docker_up.sh
@@ -7,13 +7,15 @@ set -ev
 if [ "$TRAVIS" = true ]
 then
   iface=eth0
+  docker_ip=$(ifconfig eth0 | grep 'inet ' | awk '{print $2}' | cut -d':' -f2)
 else
   iface=$(ifconfig | ./scripts/active_ifaces.sh | head -n 1 | cut -d ':' -f1)
+  docker_ip=$(ifconfig ${iface} | grep 'inet ' | awk '{print $2}')
 fi
 
 echo Detected active network interface ${iface}
 
-export DOCKER_IP=$(ifconfig ${iface} | grep 'inet ' | awk '{print $2}')
+export DOCKER_IP=${docker_ip}
 
 echo Setting DOCKER_IP to $DOCKER_IP
 
