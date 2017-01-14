6ff60ddd7eaa28e52d70d773860b669d80f56aa9
Dan Swain <dan.t.swain@gmail.com>
2017-01-14 11:30:55 -0500

    Use eth0 for travis

--- scripts/active_ifaces.sh
+++ scripts/active_ifaces.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/awk -f
 # print only active ifaces from ifconfig
 #    from http://unix.stackexchange.com/questions/103241/how-to-use-ifconfig-to-show-active-interface-only
-#    (we need a version that does not use pcregrep, which is not installed
-#      on travis build images)
+#    (using a version without pcregrep because that may not be available)
 BEGIN            { print_it = 0  }
 /status: active/ { print_it = 1  }
 /^($|[^\t])/     { if(print_it) print buffer; buffer = $0; print_it = 0  }
--- scripts/docker_up.sh
+++ scripts/docker_up.sh
@@ -4,13 +4,17 @@
 
 set -ev
 
-echo $(ifconfig)
+if [ "$TRAVIS" = true ]
+then
+  iface=eth0
+else
+  iface=$(ifconfig | ./scripts/active_ifaces.sh | head -n 1 | cut -d ':' -f1)
+fi
 
-iface=$(ifconfig | ./scripts/active_ifaces.sh | head -n 1 | cut -d ':' -f1)
 echo Detected active network interface ${iface}
 
 export DOCKER_IP=$(ifconfig ${iface} | grep 'inet ' | awk '{print $2}')
 
-echo The Docker IP is $DOCKER_IP
+echo Setting DOCKER_IP to $DOCKER_IP
 
 docker-compose up -d
