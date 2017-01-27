1a4f7819c0411470d245323cd6d0df5229b6e6e5
Dan Swain <dan.t.swain@gmail.com>
2017-01-26 20:35:20 -0500

    Add config.exs to the package, version bump
    This is needed because the config module reads config.exs to generate
    documentation.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KafkaEx.Mixfile do
   def project do
     [
       app: :kafka_ex,
-      version: "0.6.2",
+      version: "0.6.3",
       elixir: "~> 1.0",
       dialyzer: [
         plt_add_deps: :transitive,
@@ -51,7 +51,7 @@ defmodule KafkaEx.Mixfile do
 
   defp package do
     [maintainers: ["Abejide Ayodele", "Dan Swain", "Jack Lund"],
-     files: ["lib", "mix.exs", "README.md"],
+     files: ["lib", "config/config.exs", "mix.exs", "README.md"],
      licenses: ["MIT"],
      links: %{"Github" => "https://github.com/kafkaex/kafka_ex"}]
   end
