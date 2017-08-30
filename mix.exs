ad20ee0e9fc2eb87c7433d429c868df3355a3c52
Dan Swain <dan.t.swain@gmail.com>
2017-08-30 10:42:29 -0400

    Version bump for 0.8.0 release :tada:

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KafkaEx.Mixfile do
   def project do
     [
       app: :kafka_ex,
-      version: "0.7.0",
+      version: "0.8.0",
       elixir: "~> 1.1",
       dialyzer: [
         plt_add_deps: :transitive,
