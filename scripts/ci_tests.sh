d251b32d41a8aa8f35b247edd92179d2854efa36
Joshua Scott <joshua.scott@gmail.com>
2018-10-10 19:23:52 -0500

    Add credo to CI tests
    Fixes #100

--- scripts/ci_tests.sh
+++ scripts/ci_tests.sh
@@ -6,7 +6,14 @@
 #
 # This script could be used for local testing as long as COVERALLS is not set.
 
+set -e
+
 export MIX_ENV=test
+
+if [ "$CREDO" = true ]
+then
+  mix credo
+fi
 
 if [ "$COVERALLS" = true ]
 then
@@ -16,14 +23,6 @@ else
   TEST_COMMAND=test
 fi
 
-mix "$TEST_COMMAND" --include integration --include consumer_group --include server_0_p_9_p_0 
+mix "$TEST_COMMAND" --include integration --include consumer_group --include server_0_p_9_p_0 --include server_0_p_9_p_0 ||
+  mix "$TEST_COMMAND" --include integration --include consumer_group --include server_0_p_9_p_0 --include server_0_p_9_p_0
 
-# sometimes the first test run fails due to broker issues and we need to run it again
-#    (we should strive to remove this but it is necessary for now)
-if [ $? -eq 0 ]
-then
-  echo "First tests passed, skipping repeat"
-else
-  echo "Repeating tests"
-  mix "$TEST_COMMAND" --include integration --include consumer_group --include server_0_p_9_p_0 --include server_0_p_8_p_0
-fi
