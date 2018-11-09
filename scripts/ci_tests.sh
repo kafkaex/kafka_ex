bbf6088d611cd0c8fa9e84d51b5cd5988693555b
Joshua Scott <joshua.scott@gmail.com>
2018-11-08 18:58:02 -0600

    Fix warning

--- scripts/ci_tests.sh
+++ scripts/ci_tests.sh
@@ -26,4 +26,4 @@ fi
 INCLUDED_TESTS="--include integration --include consumer_group --include server_0_p_10_p_1  --include server_0_p_9_p_0 --include server_0_p_8_p_0"
 
 # Retry if it doesn't work the first time
-mix "$TEST_COMMAND" $INCLUDED_TESTS || mix "$TEST_COMMAND" $INCLUDED_TESTS
+mix "$TEST_COMMAND" "$INCLUDED_TESTS" || mix "$TEST_COMMAND" "$INCLUDED_TESTS"
