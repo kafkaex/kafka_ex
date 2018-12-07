310cd47045d9ca7f9f93d9744adf45a633cb1c0d
Joshua Scott <joshua.scott@gmail.com>
2018-12-07 08:16:39 -0600

    Fix CI scripts - ignore ebert's bad suggestion

--- scripts/ci_tests.sh
+++ scripts/ci_tests.sh
@@ -6,7 +6,7 @@
 #
 # This script could be used for local testing as long as COVERALLS is not set.
 
-set -e
+set -ex
 
 export MIX_ENV=test
 
@@ -23,7 +23,7 @@ else
   TEST_COMMAND=test
 fi
 
-INCLUDED_TESTS="--include integration --include consumer_group --include server_0_p_10_and_later  --include server_0_p_9_p_0 --include server_0_p_8_p_0"
+INCLUDED_TESTS="--include integration --include consumer_group --include server_0_p_10_and_later --include server_0_p_9_p_0 --include server_0_p_8_p_0"
 
 # Retry if it doesn't work the first time
-mix "$TEST_COMMAND" "$INCLUDED_TESTS" || mix "$TEST_COMMAND" "$INCLUDED_TESTS"
+mix "$TEST_COMMAND" $INCLUDED_TESTS || mix "$TEST_COMMAND" $INCLUDED_TESTS
