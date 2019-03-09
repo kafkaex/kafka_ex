64dc99cf90dcef9295fd3ad390e667ce3d7eace6
Jehan Bruggeman <jehan.bruggeman@euranova.eu>
2019-03-09 07:29:38 +0100

    Run "mix format" on the project

--- test/protocol/join_group_test.exs
+++ test/protocol/join_group_test.exs
@@ -3,43 +3,42 @@ defmodule KafkaEx.Protocol.JoinGroup.Test do
   alias KafkaEx.Protocol.JoinGroup
 
   test "create_request creates a valid join group request" do
-    good_request =
-      <<
-        # Preamble
-        11::16,
-        0::16,
-        42::32,
-        9::16,
-        "client_id"::binary,
-        # GroupId
-        5::16,
-        "group"::binary,
-        # SessionTimeout
-        3600::32,
-        # MemberId
-        9::16,
-        "member_id"::binary,
-        # ProtocolType
-        8::16,
-        "consumer"::binary,
-        # GroupProtocols array size
-        1::32,
-        # Basic strategy, "roundrobin" has some restrictions
-        6::16,
-        "assign"::binary,
-        # length of metadata
-        32::32,
-        # v0
-        0::16,
-        # Topics array
-        2::32,
-        9::16,
-        "topic_one"::binary,
-        9::16,
-        "topic_two"::binary,
-        # UserData
-        0::32
-      >>
+    good_request = <<
+      # Preamble
+      11::16,
+      0::16,
+      42::32,
+      9::16,
+      "client_id"::binary,
+      # GroupId
+      5::16,
+      "group"::binary,
+      # SessionTimeout
+      3600::32,
+      # MemberId
+      9::16,
+      "member_id"::binary,
+      # ProtocolType
+      8::16,
+      "consumer"::binary,
+      # GroupProtocols array size
+      1::32,
+      # Basic strategy, "roundrobin" has some restrictions
+      6::16,
+      "assign"::binary,
+      # length of metadata
+      32::32,
+      # v0
+      0::16,
+      # Topics array
+      2::32,
+      9::16,
+      "topic_one"::binary,
+      9::16,
+      "topic_two"::binary,
+      # UserData
+      0::32
+    >>
 
     request =
       JoinGroup.create_request(42, "client_id", %JoinGroup.Request{
