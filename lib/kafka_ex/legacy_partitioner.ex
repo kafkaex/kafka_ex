2ffc11c04f57f2767ae1a3f5ff58345351489c8a
Michael Parrott <michael.parrott94@gmail.com>
2020-03-16 13:16:43 -0400

    Move old default partitioner code to a legacy partitioner module

---
+++ lib/kafka_ex/legacy_partitioner.ex
@@ -0,0 +1,79 @@
+defmodule KafkaEx.LegacyPartitioner do
+  @moduledoc """
+  Legacy default partitioner implementation.
+
+  This "legacy partitioner" used to be the default partitioner for KafkaEx
+  when a message key is set and a partition isn't. This was intended to match
+  the behaviour of the default Java client, however there was some small
+  differences. This legacy partitioner keeps the previous behaviour, and the
+  current default partitioner now correctly matches the Java client.
+  """
+  use KafkaEx.Partitioner
+  alias KafkaEx.Partitioner
+  alias KafkaEx.Protocol.Produce.Request, as: ProduceRequest
+  alias KafkaEx.Protocol.Metadata.Response, as: MetadataResponse
+  alias KafkaEx.Utils.Murmur, as: Murmur
+  require Logger
+
+  @spec assign_partition(
+          request :: %ProduceRequest{},
+          metadata :: %MetadataResponse{}
+        ) :: %ProduceRequest{}
+  def assign_partition(%ProduceRequest{partition: partition} = request, _)
+      when is_number(partition) do
+    request
+  end
+
+  def assign_partition(%ProduceRequest{partition: nil} = request, metadata) do
+    case Partitioner.get_key(request) do
+      {:ok, nil} ->
+        assign_partition_randomly(request, metadata)
+
+      {:ok, key} ->
+        assign_partition_with_key(request, metadata, key)
+
+      {:error, reason} ->
+        Logger.warn(
+          "#{__MODULE__}: couldn't assign partition due to #{inspect(reason)}"
+        )
+
+        assign_partition_randomly(request, metadata)
+    end
+  end
+
+  @spec assign_partition_randomly(
+          request :: %ProduceRequest{},
+          metadata :: %MetadataResponse{}
+        ) :: %ProduceRequest{}
+  defp assign_partition_randomly(
+         %ProduceRequest{topic: topic} = request,
+         metadata
+       ) do
+    partition_id =
+      case MetadataResponse.partitions_for_topic(metadata, topic) do
+        [] -> 0
+        list -> Enum.random(list)
+      end
+
+    %{request | partition: partition_id}
+  end
+
+  @spec assign_partition_with_key(
+          request :: %ProduceRequest{},
+          metadata :: %MetadataResponse{},
+          key :: binary
+        ) :: %ProduceRequest{}
+  defp assign_partition_with_key(
+         %ProduceRequest{topic: topic} = request,
+         metadata,
+         key
+       ) do
+    hash = Murmur.umurmur2_legacy(key)
+
+    partitions_count =
+      metadata |> MetadataResponse.partitions_for_topic(topic) |> length()
+
+    partition_id = rem(hash, partitions_count)
+    %{request | partition: partition_id}
+  end
+end
--- test/kafka_ex/utils/murmur_test.exs
+++ test/kafka_ex/utils/murmur_test.exs
@@ -3,35 +3,39 @@ defmodule KafkaEx.Utils.MurmurTest do
 
   alias KafkaEx.Utils.Murmur
 
-  test "correctly encodes strings" do
-    assert Murmur.murmur2("rule") == -1_673_595_344
+  test "murmur2 correctly encodes strings" do
+    # Taken from https://github.com/apache/kafka/blob/8ab0994919752cd4870e771221ba934a6a539a67/clients/src/test/java/org/apache/kafka/common/utils/UtilsTest.java#L66-L78
+    assert Murmur.murmur2("21") == -973932308
+    assert Murmur.murmur2("foobar") == -790332482
+    assert Murmur.murmur2("a-little-bit-long-string") == -985981536
+    assert Murmur.murmur2("a-little-bit-longer-string") == -1486304829
+    assert Murmur.murmur2("lkjh234lh9fiuh90y23oiuhsafujhadof229phr9h19h89h8") == -58897971
+    assert Murmur.murmur2("abc") == 479470107
+  end
+
+  test "umurmur2 correctly encodes strings" do
     assert Murmur.umurmur2("rule") == 473_888_304
-
-    assert Murmur.murmur2("monkey") == 385_264_353
     assert Murmur.umurmur2("monkey") == 385_264_353
-
-    assert Murmur.murmur2("hover") == -1_982_829_826
     assert Murmur.umurmur2("hover") == 164_653_822
-
-    assert Murmur.murmur2("guest") == 1_235_690_374
     assert Murmur.umurmur2("guest") == 1_235_690_374
-
-    assert Murmur.murmur2("necklace") == -515_547_202
     assert Murmur.umurmur2("necklace") == 1_631_936_446
-
-    assert Murmur.murmur2("storm") == 393_248_174
     assert Murmur.umurmur2("storm") == 393_248_174
-
-    assert Murmur.murmur2("paint") == -1_653_751_818
     assert Murmur.umurmur2("paint") == 493_731_830
-
-    assert Murmur.murmur2("agony") == -1_158_062_389
     assert Murmur.umurmur2("agony") == 989_421_259
-
-    assert Murmur.murmur2("strategic") == -758_786_001
     assert Murmur.umurmur2("strategic") == 1_388_697_647
-
-    assert Murmur.murmur2("redundancy") == 451_414_978
     assert Murmur.umurmur2("redundancy") == 451_414_978
+  end
+
+  test "legacy umurmur2 correctly encodes strings" do
+    assert Murmur.umurmur2_legacy("rule") == 2_621_371_952
+    assert Murmur.umurmur2_legacy("monkey") == 385_264_353
+    assert Murmur.umurmur2_legacy("hover") == 2_312_137_470
+    assert Murmur.umurmur2_legacy("guest") == 1_235_690_374
+    assert Murmur.umurmur2_legacy("necklace") == 3_779_420_094
+    assert Murmur.umurmur2_legacy("storm") == 393_248_174
+    assert Murmur.umurmur2_legacy("paint") == 2_641_215_478
+    assert Murmur.umurmur2_legacy("agony") == 3_136_904_907
+    assert Murmur.umurmur2_legacy("strategic") == 3_536_181_295
+    assert Murmur.umurmur2_legacy("redundancy") == 451_414_978
   end
 end
