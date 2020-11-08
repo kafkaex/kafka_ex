d3cfc042f4787e3ed0b3a410afbfa1920d48ad81
Dan Swain <dan.t.swain@gmail.com>
2020-11-07 20:36:26 -0500

    Set up kerl

--- scripts/setup_circle_ci_elixir.sh
+++ scripts/setup_circle_ci_elixir.sh
@@ -3,9 +3,10 @@
 set -ex
 
 curl -O https://raw.githubusercontent.com/kerl/kerl/master/kerl
+chmod +x kerl
 \curl -sSL https://raw.githubusercontent.com/taylor/kiex/master/install | bash -s
 
-kerl build $OTP_VERSION
-kerl install $OTP_VERSION
+./kerl build $OTP_VERSION
+./kerl install $OTP_VERSION
 
 kiex install $ELIXIR_VERSION
