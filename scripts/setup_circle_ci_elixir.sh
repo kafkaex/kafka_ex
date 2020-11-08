4360a3b0bfd02bd1daca47f6850e10cd079e5e2f
Dan Swain <dan.t.swain@gmail.com>
2020-11-07 20:34:55 -0500

    Try installing elixir

---
+++ scripts/setup_circle_ci_elixir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ex
+
+curl -O https://raw.githubusercontent.com/kerl/kerl/master/kerl
+\curl -sSL https://raw.githubusercontent.com/taylor/kiex/master/install | bash -s
+
+kerl build $OTP_VERSION
+kerl install $OTP_VERSION
+
+kiex install $ELIXIR_VERSION
