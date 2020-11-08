8210285b7c646cfbd91bf4c7dda34f9ce1de8f5d
Dan Swain <dan.t.swain@gmail.com>
2020-11-07 21:01:45 -0500

    Do you even wget?

--- scripts/setup_circle_ci_elixir.sh
+++ scripts/setup_circle_ci_elixir.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-wget -O- https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_${OTP_VERSION}-1~ubuntu~xenial_amd64.deb
+wget https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_${OTP_VERSION}-1~ubuntu~xenial_amd64.deb
 sudo dpkg -i esl-erlang_${OTP_VERSION}-1~ubuntu~xenial_amd64.deb
 
 
-wget -O- https://packages.erlang-solutions.com/erlang/debian/pool/elixir_${ELIXIR_VERSION}-1~ubuntu~xenial_all.deb
+wget https://packages.erlang-solutions.com/erlang/debian/pool/elixir_${ELIXIR_VERSION}-1~ubuntu~xenial_all.deb
 sudo dpkg -i elixir_${ELIXIR_VERSION}-1~ubuntu~xenial_all.deb
