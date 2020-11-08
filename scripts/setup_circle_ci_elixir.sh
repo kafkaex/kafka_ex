4ac3c07b09f78fa0525981f8c6424ff3dead5ccf
Dan Swain <dan.t.swain@gmail.com>
2020-11-07 21:10:01 -0500

    maybe?

--- scripts/setup_circle_ci_elixir.sh
+++ scripts/setup_circle_ci_elixir.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-set -ex
-
 wget https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_${OTP_VERSION}-1~ubuntu~xenial_amd64.deb
-sudo apt install -y ./esl-erlang_${OTP_VERSION}-1~ubuntu~xenial_amd64.deb
-
+sudo dpkg -i ./esl-erlang_${OTP_VERSION}-1~ubuntu~xenial_amd64.deb
+sudo apt-get install -f
 
 wget https://packages.erlang-solutions.com/erlang/debian/pool/elixir_${ELIXIR_VERSION}-1~ubuntu~xenial_all.deb
-sudo apt install -y ./elixir_${ELIXIR_VERSION}-1~ubuntu~xenial_all.deb
+sudo dpkg -i ./elixir_${ELIXIR_VERSION}-1~ubuntu~xenial_all.deb
+sudo apt-get install -f
