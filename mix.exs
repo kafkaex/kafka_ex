52568e7180d4e006030f3c1bd955ed22fdf0f0ee
Argonus <argonuspiotr@gmail.com>
2022-04-21 15:27:13 +0200

    MInor fixes
    - Update ssl related code and nest it into describes
    - Updates tests to support new broker version
    - Update docker-compose to create topics
    - fix check

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule KafkaEx.Mixfile do
     main_deps = [
       {:kayrock, "~> 0.1.12"},
       {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.3", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.7", only: :test, runtime: false},
       {:snappy,
        git: "https://github.com/fdmanana/snappy-erlang-nif", only: [:dev, :test]}
