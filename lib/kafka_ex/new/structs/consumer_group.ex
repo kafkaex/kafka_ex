dc9a67107550ce9077dfc0c9d4d14e6ed5b8fdf3
Argonus <argonuspiotr@gmail.com>
2023-10-07 08:28:16 +0200

    [Kayrock] Add consumer group structs

---
+++ lib/kafka_ex/new/structs/consumer_group.ex
@@ -0,0 +1,31 @@
+defmodule KafkaEx.New.Structs.ConsumerGroup do
+  @moduledoc """
+  Encapsulates what we know about consumer group
+  """
+
+  alias __MODULE__.Member
+
+  @type t :: %__MODULE__{
+          group_id: binary,
+          state: binary,
+          protocol_type: binary,
+          protocol: binary,
+          members: list(Member.t())
+        }
+
+  defstruct ~w(group_id state protocol_type protocol members)a
+
+  @spec from_describe_group_response(map) :: __MODULE__.t()
+  def from_describe_group_response(describe_group) do
+    %KafkaEx.New.ConsumerGroup{
+      group_id: describe_group.group_id,
+      state: describe_group.state,
+      protocol_type: describe_group.protocol_type,
+      protocol: describe_group.protocol,
+      members:
+        Enum.map(describe_group.members, fn group_member ->
+          Member.from_describe_group_response(group_member)
+        end)
+    }
+  end
+end
