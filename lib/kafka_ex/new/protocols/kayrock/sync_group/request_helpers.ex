80564afd51b83f19a0bedd7308ad52e612f8fbc1
Piotr Rybarczyk <9743549+Argonus@users.noreply.github.com>
2025-10-22 10:26:05 +0200

    [K] Use shared code

--- lib/kafka_ex/new/protocols/kayrock/sync_group/request_helpers.ex
+++ lib/kafka_ex/new/protocols/kayrock/sync_group/request_helpers.ex
@@ -20,4 +20,24 @@ defmodule KafkaEx.New.Protocols.Kayrock.SyncGroup.RequestHelpers do
       group_assignment: Keyword.get(opts, :group_assignment, [])
     }
   end
+
+  @doc """
+  Builds a SyncGroup request by populating the request template with extracted fields.
+  This is shared logic between V0 and V1 requests as they have identical structure.
+  """
+  @spec build_request_from_template(map(), Keyword.t()) :: map()
+  def build_request_from_template(request_template, opts) do
+    %{
+      group_id: group_id,
+      generation_id: generation_id,
+      member_id: member_id,
+      group_assignment: group_assignment
+    } = extract_common_fields(opts)
+
+    request_template
+    |> Map.put(:group_id, group_id)
+    |> Map.put(:generation_id, generation_id)
+    |> Map.put(:member_id, member_id)
+    |> Map.put(:group_assignment, group_assignment)
+  end
 end
--- lib/kafka_ex/new/protocols/kayrock/sync_group/v1_request_impl.ex
+++ lib/kafka_ex/new/protocols/kayrock/sync_group/v1_request_impl.ex
@@ -9,17 +9,6 @@ defimpl KafkaEx.New.Protocols.Kayrock.SyncGroup.Request, for: Kayrock.SyncGroup.
   alias KafkaEx.New.Protocols.Kayrock.SyncGroup.RequestHelpers
 
   def build_request(request_template, opts) do
-    %{
-      group_id: group_id,
-      generation_id: generation_id,
-      member_id: member_id,
-      group_assignment: group_assignment
-    } = RequestHelpers.extract_common_fields(opts)
-
-    request_template
-    |> Map.put(:group_id, group_id)
-    |> Map.put(:generation_id, generation_id)
-    |> Map.put(:member_id, member_id)
-    |> Map.put(:group_assignment, group_assignment)
+    RequestHelpers.build_request_from_template(request_template, opts)
   end
 end
